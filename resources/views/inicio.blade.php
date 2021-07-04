09fc20dd954cc7e4e1e5f2a8cbe10bf65f65e1dc
betosS11S <49312039+betosS11S@users.noreply.github.com>
2021-07-04 08:47:43 -0500

    A

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -4,12 +4,14 @@
     <!--main content start-->
     <section id="main-content">
         <section class="wrapper site-min-height">
-            <h3><i class="fa fa-angle-right"></i> Blank Page</h3>
+            <h3><i class="fa fa-angle-right"></i> Noticias</h3>
             <div class="row mt">
-                <div class="col-lg-12">
-                    <p>Place your content here.</p>
-                
-
+                <div class="col-lg-12 text-center">
+                    <img width="80%" class="" src="https://scontent.flim16-1.fna.fbcdn.net/v/t1.6435-9/204277952_1252342151867610_5295592094726960260_n.jpg?_nc_cat=103&ccb=1-3&_nc_sid=730e14&_nc_ohc=sLJqj6BNEkIAX85HT9X&_nc_ht=scontent.flim16-1.fna&oh=424d3d2bfee1131274fb406bb93ccb98&oe=60E7880F" alt="a">
+                </div>
+                <br>
+                <div class="col-lg-12 text-center">
+                    <img width="80%" class="" src="https://scontent.flim16-1.fna.fbcdn.net/v/t1.6435-9/207056684_1251649288603563_2036188984636409612_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=730e14&_nc_ohc=enfFbHEKc6gAX-l06gt&_nc_ht=scontent.flim16-1.fna&oh=989375971b8df47221d3956abdf1270b&oe=60E722E6" alt="a">
                 </div>
             </div>
         </section>
